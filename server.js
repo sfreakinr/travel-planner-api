c8b287d3a018774ab4aa28801710dc7df7a0f5e5
sfreakinr <subinrajwwe@gmail.com>
2025-02-09 11:52:24 +0530

    updated server

--- server.js
+++ server.js
@@ -13,5 +13,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
+  app.get("/", (req, res) => {
+    res.send("Welcome to the Travel Planner API!");
+  });
+  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
